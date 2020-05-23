991caa229bc9ca7637c682e8034ac8a41960f332
DemonLord98 <ahmedalkadhim20001@gmail.com>
2020-05-24 00:16:06 +0300

    Convert from a concept to a working program, update README

--- RC4_Algorithm/RC4_Algorithm.py
+++ RC4_Algorithm/RC4_Algorithm.py
@@ -1,36 +1,56 @@
+def utf_8_to_int(text, encoding='utf-8', errors='surrogatepass'):
+    integer = int.from_bytes(text.encode(encoding, errors), 'big')
+    return integer
+
+
+def create_int_list(list):
+    for i in range(len(list)):
+        list[i] = utf_8_to_int(list[i])
+    return list
+
+
 def swap_func(list, pos1, pos2):  
     list[pos1], list[pos2] = list[pos2], list[pos1] 
     return list
 
-KEY = [1, 2, 3, 6]
-PLAIN_TEXT = [1, 2, 2, 2]
-ciphertext = []
-# S_vector = list(range(0,256)) comment for now for testing purposes
-S_vector = list(range(0,8))
+
+plain_text = list(input("Enter text to be encrypted: "))
+key = list(input("Enter the key: "))
+
+plain_text = create_int_list(plain_text)
+key = create_int_list(key)
+
+S_vector = list(range(0,256))
 T_vector = []
-i, j= 0, 0
+cipher = []
+i, j = 0, 0
 while i < len(S_vector):
-    appe = KEY[i % len(KEY)]
+    appe = key[i % len(key)]
     T_vector.append(appe)
     i += 1
 
 i = 0
-
 while i < len(S_vector):
     j = (j + S_vector[i] + T_vector[i]) % len(S_vector)
     swap_func(S_vector, i, j)
     i += 1
 
-print (S_vector)
-i, j, x= 0, 0, 0
-while x < len(PLAIN_TEXT):
+i, j, x = 0, 0, 0
+while x < len(plain_text):
     i = (i + 1) % len(S_vector)
     j = (j + S_vector[i]) % len(S_vector)
     swap_func(S_vector, i, j)
     t = (S_vector[i] + S_vector[j]) % len(S_vector)
     key_mod = S_vector[t]
-    appe = PLAIN_TEXT[x] ^ key_mod
-    ciphertext.append(appe)
+    appe = plain_text[x] ^ key_mod
+    cipher.append(appe)
     x += 1
 
-print(ciphertext)
+
+cipher_hex = list(map(hex, cipher))
+for i in range(len(cipher_hex)):
+    cipher_hex[i] = cipher_hex[i].replace("0x", "")
+
+cipher_hex_text = " ".join(cipher_hex)
+print(f"\nCiphered text in Hex: \"{cipher_hex_text}\"")
+
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 [![Run on Repl.it](https://repl.it/badge/github/DemonLord98/RC4-Algorithm)](https://repl.it/github/DemonLord98/RC4-Algorithm)
 
 # RC4 Algorithm
+A simple RC4 cipher program written in Python.
+### How the program works
+The program currently works by recieving the text to be encoded and the key both in the form of a UTF-8 string, then convert them to integers to work with them mathematically, and finally output the ciphered text in the form of hexadecimal numbers.
+### TODO
+- [ ] Create a decrypting method
+- [ ] Enable Hex inputs
+- [ ] let the user choose to decrypt or to encrypt
